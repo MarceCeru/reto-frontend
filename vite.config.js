d68cb2a3a01b87973446619acbbd4949e75a71bc
Cerutti Marcelo <134024715+MarceCeru@users.noreply.github.com>
2024-12-15 00:05:39 -0300

    base url para deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://MarceCeru.github.io/homepage'
+  base: 'https://marceceru.github.io/reto-frontend/'
 })
